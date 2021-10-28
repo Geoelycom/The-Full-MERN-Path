e25ff0762263dbc6b00900a924128e4125eb6798
Elyan <ekenimohelyanpheta@gmail.com>
2021-10-28 15:06:14 -0700

    add new component

--- React-project1/my-app/src/index.jsx
+++ React-project1/my-app/src/index.jsx
@@ -2,15 +2,25 @@
  import ReactDOM from "react-dom";
  import faker from "faker";
  import { CommentDetail } from "./CommentDetail";
+ import { ApprovalCard } from "./ApprovalCard";
 
  const App = () => {
  return (
-    <div className="ui container comments">
-     <CommentDetail author="sam"  timeAgo="yesterday at 2:30AM" blogPost="this looks great" avatar={faker.image.avatar()} />
-     <CommentDetail author="john" timeAgo="today at 5:45pm"  blogPost="great post" avatar={faker.image.avatar()}/>
-     <CommentDetail  author="sara" timeAgo="today at 12:10pm" blogPost="interesting post" avatar={faker.image.avatar()}/>
-   </div>
+ <div className="ui container comments">
+   <ApprovalCard>
+     <CommentDetail author="sam" timeAgo="yesterday at 2:30AM" blogPost="this looks great"
+       avatar={faker.image.avatar()} />
+   </ApprovalCard>
 
+   <ApprovalCard>
+     <CommentDetail author="john" timeAgo="today at 5:45pm" blogPost="great post" avatar={faker.image.avatar()} />
+   </ApprovalCard>
+
+   <ApprovalCard>
+     <CommentDetail author="sara" timeAgo="today at 12:10pm" blogPost="interesting post"
+       avatar={faker.image.avatar()} />
+   </ApprovalCard>
+ </div>
  );
  }
 
