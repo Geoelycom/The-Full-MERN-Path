15aa69e73e0c5843694e018a24c37d06be82fa89
Elyan <ekenimohelyanpheta@gmail.com>
2021-10-28 07:51:22 -0700

    add  new component

--- React-project1/my-app/src/CommentDetail.jsx
+++ React-project1/my-app/src/CommentDetail.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import faker from "faker";
+//import faker from "faker";
 
-export const CommentDetail = () => {
+export const CommentDetail = props => {
 return (
 <div className="comment">
   <a href="/" className="avatar">
-    <img alt="avater" src={faker.image.avatar()} />
+    <img alt="avater" src={props.avatar} />
   </a>
   <div className="content">
     <a href="/" className="author">
-      Adam
+      {props.author}
     </a>
     <div className="metadata">
-      <span className="date">Today at 9:00pm</span>
+      <span className="date">{props.timeAgo}</span>
     </div>
     <div className="text">
-      Nice blog post!
+      {props.blogPost}
     </div>
   </div>
 </div>
