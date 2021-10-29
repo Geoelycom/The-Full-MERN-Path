8cc084ba60a1cd363b41f5f3476dce8951389b91
Elyan <ekenimohelyanpheta@gmail.com>
2021-10-29 12:38:51 -0700

    initialized season component

--- React-project1/my-app/src/index.jsx
+++ React-project1/my-app/src/index.jsx
@@ -1,30 +1,36 @@
- import React from "react";
- import ReactDOM from "react-dom";
- import faker from "faker";
- import { CommentDetail } from "./CommentDetail";
- import { ApprovalCard } from "./ApprovalCard";
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import { CommentDetail } from "./CommentDetail";
+import { ApprovalCard } from "./ApprovalCard";
+import { Season } from "./Season";
 
- const App = () => {
- return (
- <div className="ui container comments">
-   <ApprovalCard>
-     <CommentDetail author="sam" timeAgo="yesterday at 2:30AM" blogPost="this looks great"
-       avatar={faker.image.avatar()} />
-   </ApprovalCard>
+const App = () => {
+return (
+<div className="ui container comments">
+  <ApprovalCard>
+    <CommentDetail author="sam" timeAgo="yesterday at 2:30AM" blogPost="this looks great"
+      avatar={faker.image.avatar()} />
+  </ApprovalCard>
 
-   <ApprovalCard>
-     <CommentDetail author="john" timeAgo="today at 5:45pm" blogPost="great post" avatar={faker.image.avatar()} />
-   </ApprovalCard>
+  <ApprovalCard>
+    <CommentDetail author="john" timeAgo="today at 5:45pm" blogPost="great post" avatar={faker.image.avatar()} />
+  </ApprovalCard>
 
-   <ApprovalCard>
-     <CommentDetail author="sara" timeAgo="today at 12:10pm" blogPost="interesting post"
-       avatar={faker.image.avatar()} />
-   </ApprovalCard>
- </div>
- );
- }
+  <ApprovalCard>
+    <CommentDetail author="sara" timeAgo="today at 12:10pm" blogPost="interesting post" avatar={faker.image.avatar()} />
+  </ApprovalCard>
 
- //take the react component and display it on the screen
+  <Season/>
+    
+</div>
 
- ReactDOM.render(
- <App />, document.querySelector('#root'));
+);
+}
+
+
+
+//take the react component and display it on the screen
+
+ReactDOM.render(
+<App />, document.querySelector('#root'));
