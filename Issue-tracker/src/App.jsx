1d394e360ff9c6be5e16dc39123078921d69bf6d
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-09 13:19:12 -0800

    create babel config and add new component for testing

--- Issue-tracker/src/App.jsx
+++ Issue-tracker/src/App.jsx
@@ -1,7 +1,13 @@
+const continents = ['Africa', 'America', 'Europe', 'Asia', 'Austraila']
+const helloContinents = Array.from( continents, c =>  `hello ${c}!` )
+const message = helloContinents.join(' ')
+
+
+
 const element = (
   <div title="Outer div">
-    <h1>Hello World!</h1>
+    <h1>{message}!</h1>
   </div>
 );
 
-ReactDOM.render(element, document.querySelector('#contents'));
+ReactDOM.render(element, document.getElementById('contents'));
