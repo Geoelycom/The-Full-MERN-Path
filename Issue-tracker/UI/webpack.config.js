9376dcc2f151dddbc24d736820d779dc809d5cab
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-10 11:57:19 -0800

    Add webpack

---
+++ Issue-tracker/UI/webpack.config.js
@@ -0,0 +1,24 @@
+    const path =  require('path');
+    
+  module.exports = {
+        mode: 'development',
+        entry: './src/App.jsx',
+        output: {
+          filename: 'app.bundle.js',
+          path: path.resolve(__dirname, 'public'),
+        },
+        module: {
+          rules: [
+            {
+              test: /\.jsx?$/,
+              use: 'babel-loader',
+            },
+            
+          ],
+        },
+        resolve: {
+          modules: ['node-modules'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+
+    };
