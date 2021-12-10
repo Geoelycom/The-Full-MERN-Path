f0c38aec2ec959a71dce8b78b248df9b5c10c4ed
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-10 11:55:13 -0800

    Add issuelist component

---
+++ Issue-tracker/UI/src/IssueList.jsx
@@ -0,0 +1,19 @@
+import Issuefilter from "./IssueFilter";
+import IssueTable from "./IssueTable";
+import IssueAdd from "./IssueAdd";
+
+
+export default class IssueList extends React.Component{
+  render(){
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+          <Issuefilter />
+          <hr/>
+          <IssueTable />
+          <hr/>
+          <IssueAdd />
+      </React.Fragment> 
+    )
+  }
+}
