3c5c210622196d2c6772c0ba151765bcf28b852f
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-13 16:28:48 -0800

    replaced files

--- Issue-tracker/UI/src/App.jsx
+++ Issue-tracker/UI/src/App.jsx
@@ -1,11 +1,16 @@
 /* globals React  ReactDOM*/
-import IssueList from "./IssueList"
-const element = <IssueList />
+import "babel-polyfill";
+import "whatwg-fetch";
+import React from "react"
+import ReactDOM from "react-dom";
+import IssueList from "./IssueList";
 
-
-
-
-
-
+if (module.hot){
+  module.hot.accept()
+  }
+const element = <IssueList/>
 
 ReactDOM.render(element, document.getElementById('contents'));
+if (module.hot){
+  module.hot.accept()
+  }
