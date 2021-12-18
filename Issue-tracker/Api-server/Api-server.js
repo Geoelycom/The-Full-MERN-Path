530dd308936790f963d5a053bb4c5393ceecb2ae
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-17 18:07:16 -0800

    create issuedb and Add resolvers for list api

--- Issue-tracker/Api-server/Api-server.js
+++ Issue-tracker/Api-server/Api-server.js
@@ -3,9 +3,26 @@ const express = require('express')
 const { ApolloServer, gql } =  require("apollo-server-express");
 let aboutMessage = "Issue Tracker API v1.0";
 
+
+const issueDB = [
+  {
+    id: 1, status: 'New', owner: 'Elyan', effort: 5, 
+    created: new Date('2021-12-21'), due: undefined,
+    title: 'Error in console when clicking Add',
+  },
+
+  {
+    id: 2, status: 'Assigned', owner: 'Uduak', effort: 25, 
+    created: new Date('2021-12-22'), due: new Date('2021-12-30'),
+    title: 'Missing bottom border on panel',
+  }
+]
+
+
 const resolvers = {
   Query: {
     about: () => aboutMessage,
+    issueList,
   },
   Mutation: {
     setAboutMessage,
@@ -14,6 +31,10 @@ const resolvers = {
 
 function setAboutMessage( _, { message }){
   return aboutMessage = message;
+}
+
+function issueList(){
+  return issueDB;
 }
 
 const server = new ApolloServer({
