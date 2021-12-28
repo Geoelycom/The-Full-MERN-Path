0e08070e028eb81105d9ccb31665d3d0c5b00ac8
Elyan <ekenimohelyanpheta@gmail.com>
2021-12-28 06:27:50 -0800

    add changes to root file

--- Issue-tracker/UI/src/App.jsx
+++ Issue-tracker/UI/src/App.jsx
@@ -7,6 +7,22 @@ import IssueList from "./IssueList";
 
 const element = <IssueList/>
 
+function IssueRow(props){
+  const issue = props.issue;
+  return (
+    <tr>
+      <td>{issue.id}</td>
+      <td>{issue.status}</td>
+      <td>{issue.owner}</td>
+      <td>{issue.created}</td>
+      <td>{issue.effort}</td>
+      <td>{issue.due}</td>
+      <td>{issue.title}</td>
+    </tr>
+  )
+}
+
+
 ReactDOM.render(element, document.getElementById('contents'));
 if (module.hot){
   module.hot.accept()
