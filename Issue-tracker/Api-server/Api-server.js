26b8a0ea42d421863adea496331a6361ad186db0
Elyan <ekenimohelyanpheta@gmail.com>
2022-02-04 17:58:22 -0800

    Add user input validations

--- Issue-tracker/Api-server/Api-server.js
+++ Issue-tracker/Api-server/Api-server.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const express = require('express')
-const { GraphQLScalarType } = require ('graphql')
-const { ApolloServer } =  require("apollo-server-express");
+const { GraphQLScalarType } = require('graphql')
+const { ApolloServer, UserInputError } = require("apollo-server-express");
 const { Kind } = require('graphql/language')
-
 
 
 let aboutMessage = "Issue Tracker API v1.0";
@@ -11,30 +10,54 @@ let aboutMessage = "Issue Tracker API v1.0";
 const GraphQLDate = new GraphQLScalarType({
   name: 'GraphQLDate',
   description: 'A Date() type in GraphQL as a scalar',
-  serialize(value){
+  serialize(value) {
     return value.toISOString();
   },
 
-  parseValue(value){
-     return new Date(value);
+  parseValue(value) {
+    const dateValue = new Date(value)
+    return isNaN(dateValue) ? undefined : dateValue;
   },
 
-  parseLiteral(ast){
-     return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
+  parseLiteral(ast) {
+    if (ast.kind == Kind.STRING) {
+      const value = new Date(ast.value)
+      return isNaN(value) ? undefined : value;
+    }
   }
 
 })
 
-const issueDB = [
-  {
-    id: 1, status: 'New', owner: 'Elyan', effort: 5, 
-    created: new Date('2021-12-21'), due: undefined,
+function validateIssue(issue) {
+  const errors = [];
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be at least 3 characters long.');
+  }
+  if (issue.status == 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"');
+  }
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors })
+  }
+}
+
+const issueDB = [{
+    id: 1,
+    status: 'New',
+    owner: 'Elyan',
+    effort: 5,
+    created: new Date('2021-12-21'),
+    due: undefined,
     title: 'Error in console when clicking Add',
   },
 
   {
-    id: 2, status: 'Assigned', owner: 'Uduak', effort: 25, 
-    created: new Date('2021-12-22'), due: new Date('2021-12-30'),
+    id: 2,
+    status: 'Assigned',
+    owner: 'Uduak',
+    effort: 25,
+    created: new Date('2021-12-22'),
+    due: new Date('2021-12-30'),
     title: 'Missing bottom border on panel',
   }
 ]
@@ -52,33 +75,36 @@ const resolvers = {
   GraphQLDate,
 };
 
-function setAboutMessage( _, { message }){
+function setAboutMessage(_, { message }) {
   return aboutMessage = message;
 }
 
-function issueAdd (_, { issue }){
- issue.created = new Date();
- issue.id = issueDB.length + 1;
-  if(issue.status == undefined) {
-    issue.status = 'New';
-    issueDB.push(issue);
-    }
-    return issue;
-  }
+function issueAdd(_, { issue }) {
+  validateIssue(issue);
+  issue.created = new Date();
+  issue.id = issueDB.length + 1;
+  issueDB.push(issue);
+  return issue;
+}
 
-function issueList(){
+function issueList() {
   return issueDB;
 }
 
 const server = new ApolloServer({
-  typeDefs : fs.readFileSync('../Api-server/schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync('../Api-server/schema.graphql', 'utf-8'),
   resolvers,
+  formatError: error => {
+    console.log(error)
+    return error;
+  }
 });
 
-const app  = express()
+const app = express()
 app.use(express.static('public'))
-server.applyMiddleware({ app, path: '/graphql' })
+server.applyMiddleware({app, path: '/graphql' })
 
 app.listen(3000, () => {
   console.log('server started on port 3000')
 })
+
