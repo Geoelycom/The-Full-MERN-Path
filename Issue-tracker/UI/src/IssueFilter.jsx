c87501663382d4a5f853da19e35f7fef462a910d
Elyan <ekenimohelyanpheta@gmail.com>
2022-03-04 08:26:12 -0800

    changed links to use React-router Links and NavLinks

--- Issue-tracker/UI/src/IssueFilter.jsx
+++ Issue-tracker/UI/src/IssueFilter.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line react/prefer-stateless-function
 export default function Issuefilter() {
   return (
     <div>
-      <a href="/#/issues">All Issues </a>
+      <Link to="/issues">All Issues </Link>
       {' | '}
-      <a href="/#/issues?status=New">New Issues </a>
+      <Link to={{ pathname: '/issues', search: '?status=New' }}>New Issues </Link>
       {' | '}
-      <a href="/#/issues?status=Assigned">Assigned Issues </a>
+      <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>Assigned Issues </Link>
     </div>
   );
 }
