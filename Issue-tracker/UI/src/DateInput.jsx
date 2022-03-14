00d7efc19cc35bd309073c80f0be995266f4f518
Elyan <ekenimohelyanpheta@gmail.com>
2022-03-14 16:30:48 -0700

    add date component

---
+++ Issue-tracker/UI/src/DateInput.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+function displayFormat(date) {
+  return (date != null) ? date.toDateString().substr(0, 10) : '';
+}
+
+function editFormat(date) {
+  return (date != null) ? date.toISOstring().substr(0, 10) : '';
+}
+
+function unFormat(str) {
+  const val = new Date(str);
+  return Number.isNaN(val.getTime()) ? null : val;
+}
+
+export default function DateInput(props) {
+  const [values, updateValue] = useState({
+    value: editFormat(props.value), focused: false, valid: true,
+  });
+
+  function onFocus() {
+    updateValue({ focused: true });
+  }
+
+  function onBlur(e) {
+    const { value, valid: oldValid } = values;
+    const { onValidityChange, onChanged } = props;
+    const dateValue = unFormat(value);
+    const valid = value === '' || dateValue != null;
+    if (valid !== oldValid && onValidityChange) {
+      onValidityChange(e, dateValue);
+    }
+    updateValue({ focused: false, valid });
+    if (valid) {
+      onChanged(e, dateValue);
+    }
+  }
+
+  function onChange(e) {
+    if (e.target.value.match(/^[\d-]*/)) {
+      updateValue({ value: e.target.value });
+    }
+  }
+  const { value: originalValue, name } = props;
+  const { valid, focused, value } = values;
+  const className = (!valid && !focused) ? 'invalid' : null;
+  const displayValue = (focused || !valid) ? value : displayFormat(originalValue);
+  return (
+    <input
+      type="text"
+      size={20}
+      name={name}
+      className={className}
+      value={displayValue}
+      placeholder={focused ? 'yyyy-mm-dd' : null}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onChange={onChange}
+    />
+
+  );
+}
--- Issue-tracker/UI/src/NumInputs.jsx
+++ Issue-tracker/UI/src/NumInputs.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+// import React from 'react';
 
 function format(num) {
   return num != null ? num.toString() : '';
 }
+
 
 function unFormat(str) {
   const val = parseInt(str, 10);
@@ -34,3 +36,40 @@ export default function NumInputs(props) {
     />
   );
 }
+
+
+// export default class NumInputs extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: format(props.value),
+//     };
+//     this.onBlur = this.onBlur.bind(this);
+//     this.onChange = this.onChange.bind(this);
+//   }
+
+//   onChange(e) {
+//     if (e.target.value.match(/^\d*$/)) {
+//       console.log(this.setState({ value: e.target.value }));
+//     }
+//   }
+
+//   onBlur(e) {
+//     const { onChange } = this.props;
+//     const { value } = this.state;
+//     onChange(e, unFormat(value));
+//   }
+
+//   render() {
+//     const { value } = this.state;
+//     return (
+//       <input
+//         type="text"
+//         {...this.props}
+//         value={value}
+//         onBlur={this.onBlur}
+//         onChange={this.onChange}
+//       />
+//     );
+//   }
+// }
